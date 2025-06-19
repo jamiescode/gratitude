5678e0f21a148256f3204f9e8bf7a8cc6add2171
Jamie Cruwys <6881571+JamieCruwys@users.noreply.github.com>
2025-06-19 22:53:15 +0100

    Use kotlinx

--- navigation/src/main/java/com/jamiescode/showcase/navigation/ChromeCustomTabsLauncher.kt
+++ navigation/src/main/java/com/jamiescode/showcase/navigation/ChromeCustomTabsLauncher.kt
@@ -1,10 +1,10 @@
 package com.jamiescode.showcase.navigation
 
 import android.content.Context
-import android.net.Uri
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.net.toUri
 
 fun Context.launchCustomTabs(url: String) {
     val intent = CustomTabsIntent.Builder().build()
-    intent.launchUrl(this, Uri.parse(url))
+    intent.launchUrl(this, url.toUri())
 }
